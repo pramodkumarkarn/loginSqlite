49d2f77efbe61bd8c097c9e6c7a4fbc9f135b846
nirmal1064 <nirmal1064@gmail.com>
2018-09-06 23:55:57 +0530

    Created Login File

--- app/src/main/java/com/example/nirmal/loginsqlite/loginActivity.java
+++ app/src/main/java/com/example/nirmal/loginsqlite/loginActivity.java
@@ -1,7 +1,9 @@
 package com.example.nirmal.loginsqlite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class loginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class loginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void gotoRegister(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
